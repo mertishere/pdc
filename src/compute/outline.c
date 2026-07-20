461dbc9c363da40c644357ab316509b4ece3a6cc
= <mertkucukhuseyin90@gmail.com>
2026-07-20 02:38:20 +0200

    Attempted optimization for outline edge detection. TODO: Downsample large images.

--- src/compute/outline.c
+++ src/compute/outline.c
@@ -28,87 +28,51 @@ void outlineBlack(
 
     PNG png
 ) {
-    int width = png.ihdr.width;
-    int height = png.ihdr.height;
+    const int width = png.ihdr.width;
+    const int height = png.ihdr.height;
+
+    const int offsets[8][2] = {
+        {-1, -1}, {-1, 0}, {-1, 1}, 
+        {0, -1}, {0, 1},
+        {1, -1}, {1, 0}, {1, 1}
+    };
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            size_t tlp_index = (y - (y != 0)) * width + (x - (x != 0));
-            size_t tmp_index = (y - (y != 0)) * width + x;
-            size_t trp_index = (y - (y != 0)) * width + (x + (x != (width - 1)));
+            float center = computeBrightness(pixels[y * width + x]);
+            float brightness_sum = center;
+            float diff_sum = 0.0f;
 
-            size_t lp_index = y * width + (x - (x != 0));
-            size_t rp_index = y * width + (x + (x != (width - 1)));
-            size_t mp_index = y * width + x;
+            for (int i = 0; i < 8; i++) {
+                int ny = y + offsets[i][0];
+                int nx = x + offsets[i][1];
 
-            size_t blp_index = (y + (y != (height - 1))) * width + (x - (x != 0));
-            size_t bmp_index = (y + (y != (height - 1))) * width + x;
-            size_t brp_index = (y + (y != (height - 1))) * width + (x + (x != (width - 1)));
+                if (ny < 0 || ny >= height || nx < 0 || nx >= width) continue;
 
-            if(tlp_index > pixels_size) return;
-            if(trp_index > pixels_size) return;
-            if(tmp_index > pixels_size) return;
-            if(lp_index > pixels_size) return;
-            if(rp_index > pixels_size) return;
-            if(mp_index > pixels_size) return;
-            if(blp_index > pixels_size) return;
-            if(brp_index > pixels_size) return;
-            if(bmp_index > pixels_size) return;
-
-            // top left, middle and right pixel
-            Pixel tlp = pixels[tlp_index];
-            Pixel tmp = pixels[tmp_index];
-            Pixel trp = pixels[trp_index];
-
-            // left, and right pixel
-            Pixel lp = pixels[lp_index];
-            Pixel rp = pixels[rp_index];
-
-            // bottom left, middle and right pixel
-            Pixel blp = pixels[blp_index];
-            Pixel bmp = pixels[bmp_index];
-            Pixel brp = pixels[brp_index];
-
-            // middle pixel
-            Pixel p = pixels[mp_index];
-
-            float brightness = computeBrightness(p);
-            brightness += computeBrightness(tlp);
-            brightness += computeBrightness(tmp);
-            brightness += computeBrightness(trp);
-            brightness += computeBrightness(lp);
-            brightness += computeBrightness(rp);
-            brightness += computeBrightness(blp);
-            brightness += computeBrightness(bmp);
-            brightness += computeBrightness(brp);
-            brightness /= 9;
-
-            float center = computeBrightness(p);
-            float diff = 0.0f;
-
-            diff += fabsf(center - computeBrightness(tlp));
-            diff += fabsf(center - computeBrightness(tmp));
-            diff += fabsf(center - computeBrightness(trp));
-
-            diff += fabsf(center - computeBrightness(lp));
-            diff += fabsf(center - computeBrightness(rp));
-
-            diff += fabsf(center - computeBrightness(blp));
-            diff += fabsf(center - computeBrightness(bmp));
-            diff += fabsf(center - computeBrightness(brp));
-            diff /= 8.0f;
-
-            if((size_t)(y * width + x) >= output_size) return;
-            if (diff > 15.0f && brightness < 220.0f) {
-                output[y * width + x].rgb.r = RGB_BLACK;
-                output[y * width + x].rgb.g = RGB_BLACK;
-                output[y * width + x].rgb.b = RGB_BLACK;
-            } else {
-                output[y * width + x].rgb.r = RGB_WHITE;
-                output[y * width + x].rgb.g = RGB_WHITE;
-                output[y * width + x].rgb.b = RGB_WHITE;
+                size_t n_index = ny * width + nx;
+                if(n_index > pixels_size) continue;
+                Pixel neighbor = pixels[n_index];
+                float neighbor_brightness = computeBrightness(neighbor);
+                brightness_sum += neighbor_brightness;
+                diff_sum += fabsf(center - neighbor_brightness);
             }
-            output[y * width + x].rgb.a = 255;
+
+            float brightness = brightness_sum / 9.0f;
+            float diff = diff_sum / 8.0f;
+
+            // Apply threshold
+            size_t oi = y * width + x;
+            if (oi >= output_size) continue;
+            if (diff > 15.0f && brightness < 220.0f) {
+                output[oi].rgb.r = RGB_BLACK;
+                output[oi].rgb.g = RGB_BLACK;
+                output[oi].rgb.b = RGB_BLACK;
+            } else {
+                output[oi].rgb.r = RGB_WHITE;
+                output[oi].rgb.g = RGB_WHITE;
+                output[oi].rgb.b = RGB_WHITE;
+            }
+            output[oi].rgb.a = 255;
         }
     }
 
